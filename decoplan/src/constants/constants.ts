f989d06b1e27f2464236296de2a243ca9dd6034a
Rewqaf41 <danyburdin@gmail.com>
2024-10-29 15:03:32 +0300

    Auth config

--- decoplan/src/constants/constants.ts
+++ decoplan/src/constants/constants.ts
@@ -1,2 +1,3 @@
-export const API_URL = 'http://localhost/api/v1/'
+export const API_URL = 'http://auth.localhost/api/v1/'
+export const WS_URL = 'ws://localhost:3001/v1'
 export const IS_CLIENT = typeof window !== 'undefined'
