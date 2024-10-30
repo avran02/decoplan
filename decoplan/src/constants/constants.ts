73ddea318ac0e95d0d2dc8cbdf4e7e9eef4d8d9a
Rewqaf41 <danyburdin@gmail.com>
2024-10-30 11:42:34 +0300

    UseWebSocet

--- decoplan/src/constants/constants.ts
+++ decoplan/src/constants/constants.ts
@@ -1,3 +1,3 @@
 export const API_URL = 'http://auth.localhost/api/v1/'
-export const WS_URL = 'ws://localhost:3001/v1'
+export const WS_URL = 'ws://ws.localhost/connect/'
 export const IS_CLIENT = typeof window !== 'undefined'
