1bde7ccbca3e2b67e59e9aef003382c45038d9fa
Rewqaf41 <danyburdin@gmail.com>
2024-11-02 11:39:40 +0300

    fix save message format for map according to message

--- decoplan/src/hooks/useChatWebSocket.ts
+++ decoplan/src/hooks/useChatWebSocket.ts
@@ -6,45 +6,39 @@ import useWebSocket, { ReadyState } from 'react-use-websocket'
 
 const useChatWebSocket = (token: string) => {
   const [messages, setMessages] = useState<IMessage[]>([]);
-  const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(WS_URL, {
+  const { lastMessage, sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(WS_URL, {
     onOpen: () => console.log('WebSocket Connection Opened'),
     onClose: () => console.log('WebSocket Connection Closed'),
     queryParams: { token },
     shouldReconnect: () => true,
   });
 
-  // Обновление сообщений при получении нового сообщения
   useEffect(() => {
     if (lastJsonMessage) {
-      setMessages((prev) => [...prev, lastJsonMessage as IMessage]);
+      setMessages(lastJsonMessage as IMessage[]);
     }
   }, [lastJsonMessage]);
 
-  // Функция для подключения
   const connect = useCallback(() => {
     if (readyState !== ReadyState.OPEN) {
       console.log('Connecting WebSocket...');
     }
   }, [readyState]);
 
-  // Функция для отключения
   const disconnect = useCallback(() => {
     if (readyState === ReadyState.OPEN) {
       sendJsonMessage({ type: 'disconnect' });
     }
   }, [readyState, sendJsonMessage]);
 
-  // Отправка нового сообщения
   const sendMessage = useCallback((messageData: INewMessageDto) => {
     sendJsonMessage({ act: UserMessages.USER_SEND_MESSAGE, payload: messageData });
   }, [sendJsonMessage]);
 
-  // Получение списка сообщений
   const fetchMessages = useCallback((data: IAskMessagesDto) => {
     sendJsonMessage({ act: UserMessages.USER_GET_MESSAGES, payload: data });
   }, [sendJsonMessage]);
 
-  // Удаление сообщения
   const deleteMessage = useCallback((data: IDeleteMessageDto) => {
     sendJsonMessage({ act: UserMessages.USER_DELETE_MESSAGE, payload: data });
   }, [sendJsonMessage]);
