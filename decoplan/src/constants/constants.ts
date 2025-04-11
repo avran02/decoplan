4b4cc4e6082669051c77c57812ad284fd3e22536
Rewqaf41 <danyburdin@gmail.com>
2025-04-11 18:54:22 +0300

    some constants

--- decoplan/src/constants/constants.ts
+++ decoplan/src/constants/constants.ts
@@ -1,3 +1,4 @@
-export const API_URL = 'http://auth.localhost/api/v1/'
+export const AUTH_URL = 'http://auth.localhost/api/v1/'
+export const USERS_CHATS_URL = 'http://localhost/api/v1/'
 export const WS_URL = 'ws://localhost:8888/connect'
 export const IS_CLIENT = typeof window !== 'undefined'
