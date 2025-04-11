81e89be9b3acb5519f5546e4c4a72afdde35d168
Rewqaf41 <danyburdin@gmail.com>
2025-04-11 18:54:06 +0300

    sidebar

---
+++ decoplan/src/components/layout/sidebar/sidebar.data.ts
@@ -0,0 +1,12 @@
+import { MessagesSquare, Settings } from 'lucide-react'
+
+export const MENU = [
+	{
+		url: '/chat',
+		icon: MessagesSquare,
+	},
+	{
+		url: '/settings',
+		icon: Settings,
+	},
+]
